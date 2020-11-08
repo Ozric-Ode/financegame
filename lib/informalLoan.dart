471a3210d0fef5333203d722e6d19a54a9146108
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-11-08 14:08:33 +0530

    created setup

--- lib/informalLoan.dart
+++ lib/informalLoan.dart
@@ -19,46 +19,107 @@ class _InformalLoanState extends State<InformalLoan> {
   }
 
   Widget build(BuildContext context) {
+    double amount = 0.0;
+    int years = 0;
+    double interestRate = 11;
+    double amountPayble = 0.0;
+    void update() {
+      setState(() {
+        amount = double.parse(myController.text);
+        years = int.parse(myController2.text);
+        amountPayble = amount + (amount * years * interestRate) / 100;
+        print(amountPayble);
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text("Dhanna Seth & Family")),
       body: Container(
         child: Center(
+          // child: TextField(
+          //   onChanged: (String str) {
+          //     print(str);
+          //   },
+          // ),
+          //child: Text('Hello'),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text("Amount : -"),
-                  // TextField(
-                  //   controller: myController,
-                  //   decoration: InputDecoration(
-                  //     border: OutlineInputBorder(),
-                  //     labelText: 'Enter Amount',
-                  //   ),
-                ],
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Amount : -"),
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: myController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Amount',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Row(
-                children: <Widget>[
-                  Text("Interest "),
-                  TextField(
-                    onChanged:(String str) {
-                      print(str);
-                    },
-             
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Duration -"),
+                    Container(
+                      width: 200,
+                      child: TextField(
+                        controller: myController2,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Duration',
+                        ),
+                        onSubmitted: (value) {
+                          
+                          print(amountPayble);
+                          update();
+                          print(amountPayble);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Interest ${interestRate} %"),
+                  ],
+                ),
+              ),
+              //Text("Total amount to be paid ${amountPayble}"),
+              Center(
+                child: RaisedButton(
+                  elevation: 30,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
                   ),
-                  Text(" Years"),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Text("Duration -"),
-                  // TextField(
-                  //   controller: myController3,
-                  //   decoration: InputDecoration(
-                  //     border: OutlineInputBorder(),
-                  //     labelText: 'Enter interest',
-                  //   ),
-                  // ),
-                ],
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 125.0, vertical: 30),
+                  color: Colors.black12,
+                  onPressed: () {
+                    update();
+                    return showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Text(
+                              "${amount} credited to your bank account. Total amount to be paid ${amountPayble}"),
+                        );
+                      },
+                    );
+                  },
+                  child: Text("Submit"),
+                ),
               ),
             ],
           ),

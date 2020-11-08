6b4e201b8791e39b20167f615c6c623f8e700529
Monikinderjit Singh <52850691+Monik09@users.noreply.github.com>
2020-11-08 11:43:50 +0530

    Goals PAge (#3)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import './LandingPage.dart';
 
 import 'infopage.dart';
 
+import './GoalsPage.dart';
 void main() {
   runApp(MyApp());
 }
@@ -11,18 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {},
+      // routes: {},
       debugShowCheckedModeBanner: false,
       title: "Finance Game",
 
-      //change the name of home according to your file class name
-      // Example: if your class name is LandingPage, then write
-
-
-      home: MyCustomForm(),
-
-
-      // home: HomeScreen(),
+      home: GoalsPage(),
     );
   }
 }

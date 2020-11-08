28038c2093958f6ba8e63f269637cb7fd0467998
Chirag-Mathur <chiragmathurindia@gmail.com>
2020-11-08 12:58:48 +0530

    just a commit

--- lib/formalloans.dart
+++ lib/formalloans.dart
@@ -3,8 +3,44 @@ import 'package:flutter/material.dart';
 class FormalLoan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("data"),),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("data"),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Home Loan",
+                ),
+                Tab(
+                  text: "SME Loan",
+                ),
+                Tab(
+                  text: "Education Loan",
+                ),
+                Tab(
+                  text: "Personal Loan",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Container(
+                child: Center(child: Text("Home Loan Info"),)
+              ),
+               Container(
+                child: Center(child: Text("SME Loan Info"),)
+              ),
+               Container(
+                child: Center(child: Text("Education Loan Info"),)
+              ),
+               Container(
+                child: Center(child: Text("Personal Loan Info"),)
+              ),
+            ],
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:financegame/formalloans.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       // Example: if your class name is LandingPage, then write
       //home: LandingPage(),
 
-      // home: HomeScreen(),
+       home: FormalLoan(),
     );
   }
 }
